3fd099a456deeb4815dfc28273ef51a93cffb39a
Kshitij Kotecha <kotechakshitij@gmail.com>
2025-04-15 15:20:46 +0530

    Admin Panel created and User Registration Completed

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,28 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Feature
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('<h1> Hey Welcome </h1>')
-    # name = 'Kshitij'
-    context = {
-        'name': 'Kshitij', 
-        'age': 19, 
-        'nationality': 'Indian'
-    }
-    return render(request, 'index.html', context)
+    # feature1 = Feature(0, 'Fast', 'Our service is very quick')
+    # feature2 = Feature(1, 'Secure', 'Your data is safe with us')
+    # feature3 = Feature(2, 'Reliable', 'Runs 24/7 without issues')
+    # feature4 = Feature(3, 'Affordable', 'Cost-effective for everyone')
 
+    # features = [feature1, feature2, feature3, feature4]
+
+    features = Feature.objects.all()
+
+    context = {
+        'name': 'Kshitij',
+        'age': 19,
+        'nationality': 'Indian',
+        'features': features  
+    }
+
+    # return render(request, 'index.html', context)
+    return render(request, 'index.html', {'features': features})
+
+
+
+def register(request):
+    if request.method == 'POST': 
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2: 
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'email already used')
+                return redirect(register)
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'User name already used')
+                return redirect(register)
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save(); 
+                return redirect('login')
+        else:
+            messages.info(request, 'Password not the same')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
 
 def counter(request):
     text = request.POST['text']
     amount_of_words = len(text.split())
     return render(request, 'counter.html', {'amount': amount_of_words})
 
-# def features(request):
-#     feature1 = Feature()
-#     feature1.id = 0
-#     feature1.name  = 'Fast'
-#     feature1.details = 'Our service is very quick'
-
-#     return render(request, 'index.html'), {'features':feature1 }
